f5d7bacf51e5669c9d84841a4c0a3968cdc94bd5
dzhumayev <dzhumayev@gmail.com>
2022-09-29 23:56:37 +0600

    solution complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+System.Console.Write("Введите элементы массива через запятую: ");
+string[] sourceArray = Console.ReadLine().Split(',');
+
+string[] resultArray = new string[0];
+for (int i = 0; i < sourceArray.Length; i++)
+{
+        if (sourceArray[i].Length <= 3)
+        {
+            Array.Resize<string>(ref resultArray, resultArray.Length + 1);
+            resultArray[resultArray.Length - 1] = sourceArray[i];
+        }
+}
+
+printArray(resultArray);
+
+void printArray(string[] array, char delimiter = ' ')
+{
+
+    if (array.Length == 0)
+    {
+        return;
+    }
+    
+    StringBuilder sb = new StringBuilder();
+    sb.Append(array[0]);
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        sb.Append($"{delimiter}{array[i]}");
+    }
+
+    System.Console.WriteLine(sb);
+}
